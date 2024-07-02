2211d3ea3ccae86ac6be667e0145bc2606e6434a
sherinstella <sherinsamsherinsam45@gmail.com>
2024-07-02 20:53:13 +0530

    second commit

--- stella/src/component/Demo1.js
+++ stella/src/component/Demo1.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Demo1 = () => {
   return (
-    <div><b>HELLO EVERYONE welcoming you</b></div>
+    <div><b>HELLO EVERYONE welcoming you and ur family</b></div>
   )
 }
 export default Demo1
